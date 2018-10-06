84cac5a868722a03c27876fb8ed85c15cafd513d
acb <acb@staticplain.local>
2018-10-06 15:42:23 +0100

    Initial Commit

---
+++ Plink/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Plink
+//
+//  Created by acb on 06/10/2018.
+//  Copyright © 2018 Kineticfactory. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
